8f9651f30b31a2c0486f7c14a3441cb3869a4798
David Torstenson <davidtorstenson@gmail.com>
2014-11-22 14:23:41 -0800

    rewrote contact form code and php file

--- pages/process.php
+++ pages/process.php
@@ -1,5 +1,5 @@
 <?php
-
+/* OLD CODE - try newer below
     $to = "davidtorstenson@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
@@ -15,5 +15,30 @@
     $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
     $send = mail($to, $subject, $body, $headers);
+*/
 
+
+//add the recipient's address here
+$myemail = 'davidtorstenson@gmail.com';
+ 
+//grab named inputs from html then post to #thanks
+if (isset($_POST['name'])) {
+$name = strip_tags($_POST['name']);
+$email = strip_tags($_POST['email']);
+$message = strip_tags($_POST['message']);
+echo "<span class=\"alert alert-success\" >Your message has been received. Thanks! Here is what you submitted:</span><br><br>";
+echo "<stong>Name:</strong> ".$name."<br>";   
+echo "<stong>Email:</strong> ".$email."<br>"; 
+echo "<stong>Message:</strong> ".$message."<br>";
+ 
+//generate email and send!
+$to = $myemail;
+$email_subject = "OYRT.com message from $name";
+$email_body = "New message from the contact page on oneyearroadtrip.com! ".
+" Here are the details:\n Name: $name \n ".
+"Email: $email\n Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email";
+mail($to,$email_subject,$email_body,$headers);
+}
 ?>
